d2ba40652de8901c3b7f32bfa55b54830e4a0c8d
tvsketch <wtfisupdennys420@gmail.com>
2026-09-20 19:03:55 -0400

    Add privacy page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -39,5 +39,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/privacy`,
+      changeFrequency: "monthly",
+      priority: 0.4,
+    },
   ]
 }
